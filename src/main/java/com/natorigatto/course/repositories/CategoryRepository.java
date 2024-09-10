114461faf61b762e595c94e4fac43c746586ab8b
Nato Rigatto <rigatto_88@hotmail.com>
2024-09-10 14:46:41 -0300

    Category

---
+++ src/main/java/com/natorigatto/course/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.natorigatto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.natorigatto.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+	
+
+}
